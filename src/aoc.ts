f6b39f35005c991416cf62b2a37c7442716efc3b
Doug Broad <dcbroad3@gmail.com>
2023-12-01 11:57:26 -0500

    Add argument checks

--- src/aoc.ts
+++ src/aoc.ts
@@ -1,5 +1,11 @@
 import { day1part1, day1part2 } from './days/1/script';
 
+const dayStr = process.argv[2];
+const partStr = process.argv[3];
+
+if (!dayStr || !partStr)
+  throw 'Specify a day (1-25) and part (1-2) to run. E.G. `npm run aoc 1 1` to run day 1, part 1.';
+
 const day = Number(process.argv[2]);
 const part = Number(process.argv[3]);
 
@@ -14,7 +20,7 @@ function runAoc(day: number, part: number): void {
       }
   }
 
-  console.log(`No script available for day ${day} part ${part}`);
+  console.log(`No script available for day ${day} part ${part}.`);
 }
 
 runAoc(day, part);
